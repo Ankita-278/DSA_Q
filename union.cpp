d24f5e7e7b644de246647015fa5d6bc3dcb7bbd0
Ankita <ankita39640@gmail.com>
2023-04-30 23:24:47 +0530

    some changes

--- union.cpp
+++ union.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+//brute force approach
 vector<int>sortedunion(vector<int>a,vector<int>b){
     int n1=a.size();
     int n2=b.size();
@@ -18,19 +18,13 @@ vector<int>sortedunion(vector<int>a,vector<int>b){
     return uni;
 }
 int main(){
-    vector<int>a,b;
-    for(int i=0;i<a.size();i++){
-        cin>>a[i];
-    }
-    for(int i=0;i<b.size();i++){
-        cin>>b[i];
-    }
-       sortedunion(a,b);
-       vector<int>temp;
+    vector<int>a={1,2,3};
+    vector<int>b={2,3,4,6};
+       vector<int>temp=sortedunion(a,b);
        for(int i=0;i<temp.size();i++){
-        cout<<temp[i];
+        cout<<temp[i]<<" ";
        }
-
+          
 
 
     return 0;
