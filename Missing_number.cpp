95e6bcbf500e76462bf733e409971aa6621b96c2
Ankita <ankita39640@gmail.com>
2023-05-05 23:24:09 +0530

    missing number

---
+++ Missing_number.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int missingNumber(int A[], int N)
+{
+     int s2=(N*(N+1))/2;
+    int sum=0;
+    for(int i=0; i<N-1; i++){
+        sum+=A[i];
+    }
+     return (s2-sum);
+    
+
+
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+   cout<< missingNumber(arr,n);
+
+    return 0;
+}
